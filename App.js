ba9d053aa4e3bfd5ab07ef5ef874d1524ced904e
katsu268 <tky2002126@stu.o-hara.ac.jp>
2021-10-19 11:27:09 +0900

    katsu_10_19_1

--- App.js
+++ App.js
@@ -6,7 +6,18 @@ import { NavigationContainer } from '@react-navigation/native';
 export default function App() {
   return (
     <NavigationContainer>
-      {/* Rest of your app code */}
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+
+
+export default App;
