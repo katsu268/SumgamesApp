13bac19d4c707f71243b8594bbf4f5c40010a07b
kairi9 <53260218+kairi9@users.noreply.github.com>
2021-10-19 11:33:40 +0900

    hasegawa_10_19_4

--- App.js
+++ App.js
@@ -25,10 +25,16 @@ class App extends Component {
 
 const HomeScreen = ({ navigation }) => {
   return (
-    
+    <Button
+      title="Go to Jane's profile"
+      onPress={() =>
+        navigation.navigate('Profile', { name: 'Jane' })
+      }
+    />
   );
-}
-
-
+};
+const ProfileScreen = ({ navigation, route }) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
 
 export default App;
