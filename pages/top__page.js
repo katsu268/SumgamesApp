2b3914ccaad7c8315d0f8e63a4c2fd151335e5c5
kairi9 <53260218+kairi9@users.noreply.github.com>
2021-10-21 14:53:19 +0900

    hasegawa_10_21_2

--- pages/top__page.js
+++ pages/top__page.js
@@ -1,24 +1,99 @@
 import * as React from "react";
-import { Header, Icon } from "react-native-elements";
+import { Tile,Icon, Card, ListItem } from "react-native-elements";
+import { ScrollView, View, Text ,Button} from 'react-native';
 import { SafeAreaProvider } from "react-native-safe-area-context";
+
+const games = [
+  {
+    name: 'ApexLegends',
+    img_uri: 'https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/apex-featured-image-16x9.jpg.adapt.crop191x100.1200w.jpg',
+    genre:'FPS',
+    detail:'test',
+    platform:'PC',
+  },
+  {
+    name: 'ApexLegends',
+    img_uri: 'https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/apex-featured-image-16x9.jpg.adapt.crop191x100.1200w.jpg',
+    genre:'FPS',
+    detail:'test',
+    platform:'PC',
+  },
+  {
+    name: 'ApexLegends',
+    img_uri: 'https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/apex-featured-image-16x9.jpg.adapt.crop191x100.1200w.jpg',
+    genre:'FPS',
+    detail:'test',
+    platform:'PC',
+  },
+  {
+    name: 'ApexLegends',
+    img_uri: 'https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/apex-featured-image-16x9.jpg.adapt.crop191x100.1200w.jpg',
+    genre:'FPS',
+    detail:'test',
+    platform:'PC',
+  },
+  {
+    name: 'ApexLegends',
+    img_uri: 'https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/apex-featured-image-16x9.jpg.adapt.crop191x100.1200w.jpg',
+    genre:'FPS',
+    detail:'test',
+    platform:'PC',
+  },
+  {
+    name: 'ApexLegends',
+    img_uri: 'https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/apex-featured-image-16x9.jpg.adapt.crop191x100.1200w.jpg',
+    genre:'FPS',
+    detail:'test',
+    platform:'PC',
+  },
+ ]
+ 
 
 const TopPage = () => {
   return (
-    <Header
-      backgroundImageStyle={{}}
-      barStyle="default"
-      centerComponent={{
-        text: "SUMGAMES",
-        style: { color: "#fff" }
-      }}
-      centerContainerStyle={{}}
-      containerStyle={{ width: '100%' }}
-      leftContainerStyle={{}}
-      linearGradientProps={{}}
-      placement="left"
-      rightContainerStyle={{}}
-      statusBarProps={{}}
-    />
+    <SafeAreaProvider>
+      <ScrollView>
+        <Tile
+          imageSrc={require('../assets/images/gamer.jpg')}
+          title="SUMGAMES"
+          titleStyle={{
+            fontSize:40,
+            fontWeight:'700',
+          }}
+          featured
+          caption="FOR ALL GAMERS"
+          height={200}
+        />
+        <View>
+          <Text style={{fontSize:24,paddingTop:10,paddingLeft:10,fontWeight:'600'}}>
+            ランキング
+          </Text>
+        </View>
+        <ScrollView horizontal={true}>
+          {games.map((u)=>{
+            return (
+              <Card containerStyle={{width:220}}>
+                <Card.Title>{u.name}</Card.Title>
+                <Card.Divider/>
+                <Card.Image source={{ uri: u.img_uri }}>
+                </Card.Image>
+                <View>
+                  <Text>
+                      {u.genre} {u.platform}
+                  </Text>
+                  <Text style={{marginBottom: 10}}>
+                    {u.detail}
+                  </Text>
+                  <Button
+                    buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                    title='Matching' />
+                </View>
+              </Card>
+            )
+          })}
+        </ScrollView>
+      </ScrollView>
+    </SafeAreaProvider>
   );
 };
 
