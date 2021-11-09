3a844ba2e6014bbe0b8bdc8690269e40f53efaaa
katsu268 <tky2002126@stu.o-hara.ac.jp>
2021-11-09 12:26:40 +0900

    katsu_11_09_01

--- pages/top__page.js
+++ pages/top__page.js
@@ -53,7 +53,9 @@ const TopPage = ({navigation}) => {
             ランキング
           </Text>
         </View>
-        <ScrollView horizontal={true}>
+
+
+        {/* <ScrollView horizontal={true}>
           {data.map((u,i)=>{
             return (
               <Card key={i} containerStyle={{width:220}}>
@@ -72,7 +74,9 @@ const TopPage = ({navigation}) => {
                 </View>
               </Card>
             )
-          })}
+          })} */}
+
+
           {/* {data.map((u,i)=>{
             return (
               <Card key={i} containerStyle={{width:220}}>
@@ -94,7 +98,10 @@ const TopPage = ({navigation}) => {
               </Card>
             )
           })} */}
-        </ScrollView>
+
+
+        {/* </ScrollView> */}
+
           <View>
             <SearchBar
               platform="ios"
