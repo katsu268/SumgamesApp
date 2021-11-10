7efc227591bd6b556fa189f42361231e67f4a856
sagaryu <tky2002121@stu.o-hara.ac.jp>
2021-11-10 11:25:25 +0900

    sagawa_11_10_2

--- pages/guest_matching.js
+++ pages/guest_matching.js
@@ -8,71 +8,136 @@ const guest_matching = () =>{
     return(
         <SafeAreaProvider>
             <ScrollView>
+                {/* 募集ゲームの画像 */}
                 <Tile
                     imageSrc={require('../assets/images/apex-image.jpg')}
                     containerStyle={{
                         marginTop: 50,
-                        height:220}}
+                        height:265}}
                 />
-                <Text
-                    style={{fontSize:25}}
-                    underlineColorAndroid="transparent"
-                    placeholder="Type something"
-                    placeholderTextColor="grey"
-                    numberOfLines={10}
-                    multiline={true}
-                    label="募集条件"
-                    labelStyle={{}}
-                    labelProps={{}}
-                    placeholder="募集条件"
-                    justifyContent="flex-start">
-                募集条件
-                </Text>
-                <Input
-                containerStyle={{}}
-                disabledInputStyle={{ background: "#ddd" }}
-                inputContainerStyle={{}}
-                inputStyle={{}}
-                label="ユーザー名"
-                labelStyle={{}}
-                labelProps={{}}
-                placeholder="ユーザー名"
-                />
-                <Input
-                containerStyle={{}}
-                disabledInputStyle={{ background: "#ddd" }}
-                inputContainerStyle={{}}
-                inputStyle={{}}
-                label="ゲームタイトル"
-                labelStyle={{}}
-                labelProps={{}}
-                placeholder="ゲームタイトル"
-                />
-                <Input
-                containerStyle={{}}
-                disabledInputStyle={{ background: "#ddd" }}
-                inputContainerStyle={{}}
-                inputStyle={{}}
-                label="募集人数"
-                labelStyle={{}}
-                labelProps={{}}
-                placeholder="募集人数"
-                />
+                {/* 募集条件全体 */}
+                <View style={{marginLeft:5}}>
+                    {/* 募集条件タイトル */}
+                    <Text
+                        style={{fontSize:30}}
+                        underlineColorAndroid="transparent"
+                        placeholder="Type something"
+                        placeholderTextColor="grey"
+                        numberOfLines={10}
+                        multiline={true}
+                        label="募集条件"
+                        labelStyle={{}}
+                        labelProps={{}}
+                        placeholder="募集条件"
+                        justifyContent="flex-start">
+                    募集条件
+                    </Text>
+                    {/* ユーザー名欄 */}
+                    <View style={{
+                        flexDirection: "row",
+                        marginTop:10
+                    }}>
+                        <Text style={{
+                            fontSize:20,
+                            marginRight:46,
+                            marginLeft:26
+                        }}>
+                            ユーザー名
+                        </Text>
+                        <Text style={{
+                            fontSize:20,
+                        }}>
+                            sample_1234
+                        </Text>
+                    </View>
+                    {/* ゲーム名欄 */}
+                    <View style={{
+                        flexDirection: "row",
+                        marginTop:10
+                    }}>
+                        <Text style={{
+                            fontSize:20,
+                            marginLeft:31.5,
+                            marginRight:56
+                        }}>
+                            ゲーム名
+                        </Text>
+                        <Text style={{
+                            fontSize:20,
+                        }}>
+                            Apex Legends
+                        </Text>
+                    </View>
+                    {/* 募集人数欄 */}
+                    <View style={{
+                        flexDirection: "row",
+                        marginTop:10
+                    }}>
+                        <Text style={{
+                            fontSize:20,
+                            marginLeft:31.5,
+                            marginRight:56
+                        }}>
+                            募集人数
+                        </Text>
+                        <Text style={{
+                            fontSize:20,
+                        }}>
+                            2
+                        </Text>
+                    </View>
+                    {/* 募集人数欄 */}
+                    <View style={{
+                        flexDirection: "row",
+                        marginTop:10
+                    }}>
+                        <Text style={{
+                            fontSize:20,
+                            // marginLeft:6,
+                            marginRight:20
+                        }}>
+                            プラットフォーム
+                        </Text>
+                        <Text style={{
+                            fontSize:20,
+                        }}>
+                            PC
+                        </Text>
+                    </View>
+                    {/* その他条件 */}
+                    <View style={{
+                        flexDirection: "row",
+                        marginTop:10
+                    }}>
+                        <Text style={{
+                            fontSize:20,
+                            marginLeft:26,
+                            marginRight:43
+                        }}>
+                            その他条件
+                        </Text>
+                        <Text style={{
+                            fontSize:20,
+                        }}>
+                            エンジョイ勢です
+                        </Text>
+                    </View>
+                </View>
+                {/* 参加不参加ボタン */}
                 <View style={{
                     flexDirection: "row",
-                    marginLeft: "20%"}}>
-                <Button title="参加"
-                        style={{
-                            justifyContent: "flex-start",
-                            marginRight: 35
-                            // width: "25%",
-                            }}/>
-                <Button title="不参加"
-                        style={{
-                            justifyContent: "flex-end",
-                            marginLeft: 65,
-                            // width: "25%",
-                            }}/>
+                    marginTop:20,
+                    marginLeft: "26%"}}>
+                    <Button title="参加"
+                            style={{
+                                justifyContent: "flex-start",
+                                marginRight: 35
+                                }}/>
+                    <Button title="不参加"
+                            style={{
+                                justifyContent: "flex-end",
+                                marginLeft: 65,
+                                }}/>
                 </View>
             </ScrollView>
         </SafeAreaProvider>
@@ -80,5 +145,3 @@ const guest_matching = () =>{
 }
 
 export default guest_matching;
-
-// Inputは本来はすべてText
--- pages/mypage.js
+++ pages/mypage.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { ScrollView, Text } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Button, Input } from "react-native-elements";
 
 const mypage =()=> {
     return(
         <SafeAreaProvider>
-            <ScrollView>
+            <ScrollView style={{
+                marginLeft:5
+            }}>
                 <Text style={{
                     fontSize:30,
-                    marginTop: 50}}>
+                    marginTop: 50,
+                    marginLeft: 10}}>
                     マイページ
                 </Text>
                 <Button
@@ -17,62 +20,80 @@ const mypage =()=> {
                 style={{
                     width: "15%",
                     marginLeft: "80%",
-                    alignItems: 'flex-end'}}/>
-                <Input
-                containerStyle={{}}
-                disabledInputStyle={{ background: "#ddd" }}
-                inputContainerStyle={{}}
-                inputStyle={{}}
-                label="ユーザー名"
-                labelStyle={{}}
-                labelProps={{}}
-                placeholder="ユーザー名"
+                    alignItems: 'flex-end',
+                    marginTop: 10}}
                 />
-                <Input
-                containerStyle={{}}
-                disabledInputStyle={{ background: "#ddd" }}
-                inputContainerStyle={{}}
-                inputStyle={{}}
-                label="募集人数"
-                labelStyle={{}}
-                labelProps={{}}
-                placeholder="募集人数"
-                />
-                <Input
-                containerStyle={{}}
-                disabledInputStyle={{ background: "#ddd" }}
-                inputContainerStyle={{}}
-                inputStyle={{}}
-                label="性別"
-                labelStyle={{}}
-                labelProps={{}}
-                placeholder="性別"
-                />
-                <Input
-                containerStyle={{}}
-                disabledInputStyle={{ background: "#ddd" }}
-                inputContainerStyle={{}}
-                inputStyle={{}}
-                label="好きなタイトル"
-                labelStyle={{}}
-                labelProps={{}}
-                placeholder="好きなタイトル"
-                />
-                <Input
-                containerStyle={{}}
-                disabledInputStyle={{ background: "#ddd" }}
-                inputContainerStyle={{}}
-                inputStyle={{}}
-                label="プレイスタイル"
-                labelStyle={{}}
-                labelProps={{}}
-                placeholder="プレイスタイル"
-                />
+                <View style={{marginLeft: "18%"}}>
+                    <View style={{
+                            flexDirection: "row",
+                            marginTop:40
+                        }}>
+                            <Text style={{
+                                fontSize:20,
+                                marginRight:35,
+                                marginLeft:15
+                            }}>
+                                ユーザー名
+                            </Text>
+                            <Text style={{
+                                fontSize:20,
+                            }}>
+                                sample_1234
+                            </Text>
+                    </View>
+                    <View style={{
+                            flexDirection: "row",
+                            marginTop:25
+                        }}>
+                            <Text style={{
+                                fontSize:20,
+                                marginRight:58,
+                                marginLeft: 41
+                            }}>
+                                性別
+                            </Text>
+                            <Text style={{
+                                fontSize:20,
+                            }}>
+                                無回答
+                            </Text>
+                    </View>
+                    <View style={{
+                            flexDirection: "row",
+                            marginTop:25
+                        }}>
+                            <Text style={{
+                                fontSize:20,
+                                marginRight:15
+                            }}>
+                                好きなタイトル
+                            </Text>
+                            <Text style={{
+                                fontSize:20,
+                            }}>
+                                BattleField
+                            </Text>
+                    </View>
+                    <View style={{
+                            flexDirection: "row",
+                            marginTop:25
+                        }}>
+                            <Text style={{
+                                fontSize:20,
+                                marginRight:15
+                            }}>
+                                プレイスタイル
+                            </Text>
+                            <Text style={{
+                                fontSize:20,
+                            }}>
+                                キルムーブ
+                            </Text>
+                    </View>
+                </View>
             </ScrollView>
         </SafeAreaProvider>
     )
 }
 
 export default mypage;
-
-// InputはTextの可能性あり
