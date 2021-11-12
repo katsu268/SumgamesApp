c1c4d4660022954a625f032c84465fa28345b368
sagaryu <tky2002121@stu.o-hara.ac.jp>
2021-11-12 10:01:00 +0900

    sagawa_11_12_1

--- pages/mypage.js
+++ pages/mypage.js
@@ -1,9 +1,13 @@
-import React from "react";
-import { ScrollView, Text, View } from "react-native";
+import React, { Component, useState } from "react";
+import { ScrollView, Text, View, Modal, Alert} from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Button, Input } from "react-native-elements";
+import { Button, colors, Input } from "react-native-elements";
+// import { background } from "native-base/lib/typescript/theme/styled-system";
+// import { Modal } from "react-native-paper";
 
 const mypage =()=> {
+    const [modalVisible, setModalVisible] = useState(false);
+
     return(
         <SafeAreaProvider>
             <ScrollView style={{
@@ -22,8 +26,107 @@ const mypage =()=> {
                     marginLeft: "80%",
                     alignItems: 'flex-end',
                     marginTop: 10}}
-                // onPress={() => navigation.navigate('change_profile')}
+                onPress={() => setModalVisible(true)}
                 />
+                <Modal
+                    animationType = "slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                        Alert.alert("Modal has been closed.");
+                        setModalVisible(!modalVisible);
+                      }}>
+                <ScrollView style={{
+                marginLeft:5,
+                backgroundColor:"white"
+                    }}>
+                    <Text style={{
+                        fontSize:30,
+                        marginTop: 50,
+                        marginLeft: 10}}>
+                        マイページ編集
+                    </Text>
+                    <Button
+                    title="保存"
+                    style={{
+                        width: "15%",
+                        marginLeft: "80%",
+                        alignItems: 'flex-end',
+                        marginTop: 10}}
+                    onPress={() => setModalVisible(!modalVisible)}
+                    />
+                    <View style={{marginLeft: "5%"}}>
+                        <View style={{
+                                flexDirection: "row",
+                                marginTop:80
+                            }}>
+                                <Text style={{
+                                    fontSize:20,
+                                    marginRight:35,
+                                    marginLeft:15
+                                }}>
+                                    ユーザー名
+                                </Text>
+                                <Input 
+                                    containerStyle={{}}
+                                    style={{
+                                    fontSize:20,
+                                }}>
+                                    sample_1234
+                                </Input>
+                        </View>
+                        <View style={{
+                                flexDirection: "row",
+                                // marginTop:5
+                            }}>
+                                <Text style={{
+                                    fontSize:20,
+                                    marginRight:58,
+                                    marginLeft: 41
+                                }}>
+                                    性別
+                                </Text>
+                                <Text style={{
+                                    fontSize:20,
+                                }}>
+                                    無回答
+                                </Text>
+                        </View>
+                        <View style={{
+                                flexDirection: "row",
+                                marginTop:25
+                            }}>
+                                <Text style={{
+                                    fontSize:20,
+                                    marginRight:15
+                                }}>
+                                    好きなタイトル
+                                </Text>
+                                <Input style={{
+                                    fontSize:20,
+                                }}>
+                                    BattleField
+                                </Input>
+                        </View>
+                        <View style={{
+                                flexDirection: "row",
+                                marginTop:5
+                            }}>
+                                <Text style={{
+                                    fontSize:20,
+                                    marginRight:15
+                                }}>
+                                    プレイスタイル
+                                </Text>
+                                <Input style={{
+                                    fontSize:20,
+                                }}>
+                                    キルムーブ
+                                </Input>
+                        </View>
+                    </View>
+                </ScrollView>
+            </Modal>
                 <View style={{marginLeft: "18%"}}>
                     <View style={{
                             flexDirection: "row",
@@ -93,6 +196,7 @@ const mypage =()=> {
                     </View>
                 </View>
             </ScrollView>
+         
         </SafeAreaProvider>
     )
 }
