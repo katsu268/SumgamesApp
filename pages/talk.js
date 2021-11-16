574c5afd61a0f168c6b25f4274943c985c0a35c0
katsu268 <tky2002126@stu.o-hara.ac.jp>
2021-11-16 14:45:12 +0900

    katsu_11_16_01

---
+++ pages/talk.js
@@ -0,0 +1,5 @@
+import { Tile, Button, Text, ThemeProvider, Input, Icon, Slider} from 'react-native-elements';
+import React ,{ Component }from 'react';
+import { View, StyleSheet, ScrollView, Image } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
