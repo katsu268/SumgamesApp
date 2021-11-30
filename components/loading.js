3730f9c099f856eb983736659377b6ab8bac7f49
satotakeru3 <tky2002017@gmail.com>
2021-11-30 14:54:30 +0900

    sato_11_30

---
+++ components/loading.js
@@ -0,0 +1,30 @@
+import { StyleSheet, View,Image} from 'react-native';
+import React from 'react'
+
+
+const Loading = (props)=>{
+  return (
+    <View style={styles.container}>
+      <Image
+          source={ require('./assets/gifs/loading_mario.gif')}
+          style={{width: props.size, height: props.size,borderRadius:"50%"}}
+          />
+    </View>
+  )
+}
+
+function Test (){
+  return (
+    <Loading size={50}/>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+  flex:1, 
+  flexDirection: 'row',
+  justifyContent : 'center',
+  alignItems : 'center'
+  },
+});
+export default Test
