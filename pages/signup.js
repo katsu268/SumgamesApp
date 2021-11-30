859e6029453baa70b1b3a697023291d27f3b28bf
garam16 <raterate0224@gmail.com>
2021-11-30 15:51:29 +0900

    munakata_11_30

--- pages/signup.js
+++ pages/signup.js
@@ -1,122 +1,56 @@
-import { Tile, Button, Text, ThemeProvider, Input, Icon, Image } from 'react-native-elements';
-import React ,{ Component }from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-
-// Your App
-const Signup = ({ navigation }) => {
+import * as React from "react"
+import {Box, Heading, VStack, FormControl, Input, Button, Center, NativeBaseProvider,} from "native-base"
+export const Example = () => {
   return (
-    <SafeAreaProvider>
-      <ScrollView>
+    <Box safeArea p="2" w="90%" maxW="290" py="8">
+      <Heading
+        size="lg"
+        color="coolGray.800"
+        _dark={{
+          color: "warmGray.50",
+        }}
+        fontWeight="semibold"
+      >
+        ようこそ
+      </Heading>
+      <Heading
+        mt="1"
+        color="coolGray.600"
+        _dark={{
+          color: "warmGray.200",
+        }}
+        fontWeight="medium"
+        size="xs"
+      >
+        サインアップしてください
+      </Heading>
+      <VStack space={3} mt="5">
+        <FormControl>
+          <FormControl.Label>Email</FormControl.Label>
+          <Input />
+        </FormControl>
+        <FormControl>
+          <FormControl.Label>Password</FormControl.Label>
+          <Input type="password" />
+        </FormControl>
+        <FormControl>
+          <FormControl.Label>Confirm Password</FormControl.Label>
+          <Input type="password" />
+        </FormControl>
+        <Button mt="2" colorScheme="indigo">
+          Sign up
+        </Button>
+      </VStack>
+    </Box>
+  )
+}
 
-        <Text style={{
-          fontSize:30,
-          marginTop: 50,
-          marginLeft: 10,
-          marginBottom: 10,
-          }}>
-          サインアップ
-        </Text>
-
-        <Input
-          containerStyle={{}}
-          disabledInputStyle={{ background: "#ddd" }}
-          inputContainerStyle={{}}
-          errorMessage="Oops! that's not correct."
-          errorStyle={{}}
-          errorProps={{}}
-          inputStyle={{}}
-          label="ユーザー名"
-          labelStyle={{}}
-          labelProps={{}}
-          leftIcon={<Icon name="person" size={20} />}
-          leftIconContainerStyle={{}}
-          rightIcon={<Icon name="close" size={20} />}
-          rightIconContainerStyle={{}}
-          placeholder="ユーザー名を入力"
-        />
-
-        <Input
-          containerStyle={{}}
-          disabledInputStyle={{ background: "#ddd" }}
-          inputContainerStyle={{}}
-          errorMessage="Oops! that's not correct."
-          errorStyle={{}}
-          errorProps={{}}
-          inputStyle={{}}
-          label="メールアドレス"
-          labelStyle={{}}
-          labelProps={{}}
-          leftIcon={<Icon name="build" size={20} />}
-          leftIconContainerStyle={{}}
-          rightIcon={<Icon name="close" size={20} />}
-          rightIconContainerStyle={{}}
-          placeholder="メールアドレスを入力"
-        />
-
-        <Input
-          containerStyle={{}}
-          disabledInputStyle={{ background: "#ddd" }}
-          inputContainerStyle={{}}
-          errorMessage="Oops! that's not correct."
-          errorStyle={{}}
-          errorProps={{}}
-          inputStyle={{}}
-          label="パスワード"
-          labelStyle={{}}
-          labelProps={{}}
-          leftIcon={<Icon name="build" size={20} />}
-          leftIconContainerStyle={{}}
-          rightIcon={<Icon name="close" size={20} />}
-          rightIconContainerStyle={{}}
-          placeholder="パスワードを入力"
-          secureTextEntry={true}
-        />
-
-        <Input
-          containerStyle={{}}
-          disabledInputStyle={{ background: "#ddd" }}
-          inputContainerStyle={{}}
-          errorMessage="Oops! that's not correct."
-          errorStyle={{}}
-          errorProps={{}}
-          inputStyle={{}}
-          label="パスワード(確認)"
-          labelStyle={{}}
-          labelProps={{}}
-          leftIcon={<Icon name="build" size={20} />}
-          leftIconContainerStyle={{}}
-          rightIcon={<Icon name="close" size={20} />}
-          rightIconContainerStyle={{}}
-          placeholder="パスワード(確認)を入力"
-          secureTextEntry={true}
-        />
-
-        <View style={{ flexDirection: 'row' }}>
-        <Button
-          buttonStyle={{ 
-            width: 150,
-           }}
-          containerStyle={{ margin: 5 }}
-          disabledStyle={{
-            borderWidth: 2,
-            borderColor: "#00F"
-          }}
-          disabledTitleStyle={{ color: "#00F" }}
-          icon={<Icon name="build" size={15} color="#0FF" />}
-          iconContainerStyle={{ background: "#000" }}
-          loadingProps={{ animating: true }}
-          loadingStyle={{}}
-          onPress={() => navigation.navigate('Login')}
-          title="サインアップ"
-          titleProps={{}}
-          titleStyle={{ marginHorizontal: 5 }}
-        />
-        </View>
-      </ScrollView>
-    </SafeAreaProvider>
-  );
-};
-
-export default Signup;
+export default () => {
+  return (
+    <NativeBaseProvider>
+      <Center flex={1} px="3">
+        <Example />
+      </Center>
+    </NativeBaseProvider>
+  )
+}
